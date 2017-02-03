8bc13e75df7bee69911314d408ab261b9a1e8c35
Clovis Mugaruka <Clovis Mugaruka>
2017-02-03 08:22:50 +0300

    updated week-2 pset2

--- Week-2/pset2.py
+++ Week-2/pset2.py
@@ -1,22 +1,23 @@
 # The below variables are used for testing purposes only.
 # while submitting for review (on Edx), they'd be removed.
 # Note: The naming convention may differ.
-balance = 3926
+balance = 4773
 annual_interest_rate = 0.2
 
 # solution starts here
-minimum = 0
-months = 12
+minimum_fixed_payment = 0
+number_of_month = 12
 
 while True:
-	monthly_interest_rate = annual_interest_rate/months
+	monthly_interest_rate = annual_interest_rate/number_of_month
 	temp = balance
 	
-	for month in range(months):
-		temp = (temp - minimum) + (monthly_interest_rate * (temp - minimum))
+	for month in range(number_of_month):
+		temp = (temp - minimum_fixed_payment) + \
+		 (monthly_interest_rate * (temp - minimum_fixed_payment))
 	
 	if temp > 0:
-		minimum += 10
+		minimum_fixed_payment += 10
 	else:
 		break
-print('Lowest Payment: {}'.format(minimum))
+print('Lowest Payment: {}'.format(minimum_fixed_payment))
